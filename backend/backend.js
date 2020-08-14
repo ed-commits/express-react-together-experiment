59b8b7de7728d6360450ce6f5ce7b8113aef7d32
ed-commits <edward.alexander.cant@gmail.com>
2020-08-14 15:57:38 +0100

    returning a list of users

--- backend/backend.js
+++ backend/backend.js
@@ -10,6 +10,17 @@ app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
 });
 
+app.get("/users", (req, res) => {
+    pool.query("SELECT * FROM users;", (err, sqlRes) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            console.log(sqlRes.rows);
+            res.status(200).send(sqlRes.rows);
+        }
+    })
+});
+
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
